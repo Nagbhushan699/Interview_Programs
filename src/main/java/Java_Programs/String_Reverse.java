d65a650a2660ba8d05e6a496b45460e624b4c0a1
Nagbhushan <nagbhushanbhalerao358@gmail.com>
2022-09-10 19:35:30 +0530

    modified commit

--- src/main/java/Java_Programs/String_Reverse.java
+++ src/main/java/Java_Programs/String_Reverse.java
@@ -3,16 +3,16 @@ package Java_Programs;
 public class String_Reverse {
 	String st=" ";
 	
-	public String rev(String str) {
+	public void  rev(String str) {
 		for (int i = str.length()-1; i >=0; i--) {
 		 System.out.print(str.charAt(i));
 		}
-		return st;
+
 	}
 
 	public static void main(String[] args) {
 		String_Reverse ob=new String_Reverse();
-	String res=	ob.rev("Nagbhushan");
+		ob.rev("Nagbhushan");
 //    System.out.println(res);
 	}
 
